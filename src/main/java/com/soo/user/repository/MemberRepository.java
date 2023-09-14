0ca83608b664ea961cde8c8241f82b284047ee6e
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-09-15 01:10:39 +0900

    feat: add member repositories

---
+++ src/main/java/com/soo/user/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.soo.user.repository;
+
+import com.soo.user.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+// 아직 데이터 저장소가 선정되지 않아서, 우선 인터페이스로 구현 클래스를 변경할 수 있도록 설계
+//데이터 저장소는 RDB, NoSQL 등등 다양한 저장소를 고민중인 상황으로 가정
+//개발을 진행하기 위해서 초기 개발 단계에서는 구현체로 가벼운 메모리 기반의 데이터 저장소 사용
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/main/java/com/soo/user/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package com.soo.user.repository;
+
+import com.soo.user.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // 동시성 문제로 ConcurrentHashMap을 써야하는데 예제이므로 그냥 Hashmap을 사용
+    private static Map<Long, Member> store = new HashMap<>();
+    // 동시성 문제로 AtomicLong같은 것을 사용해야하는데 예제이므로....
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // 결과가 없는 경우 null을 리턴하도록 Optional 사용
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
