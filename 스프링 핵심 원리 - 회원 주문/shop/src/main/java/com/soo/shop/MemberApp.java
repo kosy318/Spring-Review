bd72dad9853bb8e603fae8544adba68be0e802c8
고수연 <kosy1782@gmail.com>
2023-10-01 23:52:00 +0900

    test: test member domain not using junit

---
+++ 스프링 핵심 원리 - 회원 주문/shop/src/main/java/com/soo/shop/MemberApp.java
@@ -0,0 +1,20 @@
+package com.soo.shop;
+
+import com.soo.shop.member.Grade;
+import com.soo.shop.member.Member;
+import com.soo.shop.member.MemberService;
+import com.soo.shop.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+
+}
